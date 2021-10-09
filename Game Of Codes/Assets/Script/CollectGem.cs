ec443b53c1b0f192a3711b14af75f3eb6739cabb
Awantha Dinuranga <awanthadinuranga@gmail.com>
2021-10-10 02:06:59 +0530

    Add High Score

--- Game Of Codes/Assets/Script/CollectGem.cs
+++ Game Of Codes/Assets/Script/CollectGem.cs
@@ -2,13 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CollectGem : MonoBehaviour
 {
-    public int score;
+    int score = 0;
+
     public Text scoreText;
-    public Text highScore;
-    public bool acceptnext = false;
+    public Text bestScore;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    private void Update()
+    {
+        if(score > PlayerPrefs.GetInt("bestScore", 0))
+        {
+            PlayerPrefs.SetInt("bestScore", score);
+            bestScore.text = score.ToString();
+        }
+        
+    }
+    
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        bestScore.text = "0";
+
+
+
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -17,18 +42,22 @@ public class CollectGem : MonoBehaviour
             Destroy(collision.gameObject);
             score = score + 100;
             scoreText.text = "Score : " + score.ToString();
+
         }
         else if (collision.gameObject.CompareTag("GreenGem"))
         {
             Destroy(collision.gameObject);
             score = score + 50;
             scoreText.text = "Score : " + score.ToString();
+
         }
         else if (collision.gameObject.CompareTag("BlueGem"))
         {
             Destroy(collision.gameObject);
             score = score + 20;
             scoreText.text = "Score : " + score.ToString();
+
         }
     }
+
 }
