7c4c82c72454cba5c8efea2fcabdff61e4348c26
chathurni <buddhada_ct17012@stu.kln.ac.lk>
2021-10-09 18:23:57 +0530

    Scripts write

--- Game Of Codes/Assets/Script/Gem.cs
+++ Game Of Codes/Assets/Script/Gem.cs
@@ -8,7 +8,7 @@ public class Gem : MonoBehaviour
     public int gemValue = 1;
 
 
-    private void OnTriggerEnter2D (Collider2D other)
+    private void OnTriggerEnter (Collider other)
     {
         if (other.gameObject.CompareTag ("Player"))
         {
