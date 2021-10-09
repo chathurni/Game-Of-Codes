d3de377522a9a85485879e6dba45da916b7a9c2a
Awantha Dinuranga <awanthadinuranga@gmail.com>
2021-10-09 19:33:01 +0530

    GemCollecting

---
+++ Game Of Codes/Assets/Script/CollectGem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectGem : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("RedGem"))
+        {
+            Destroy(collision.gameObject);
+            score = score + 100;
+            scoreText.text = "Score : " + score.ToString();
+        }
+        else if (collision.gameObject.CompareTag("GreenGem"))
+        {
+            Destroy(collision.gameObject);
+            score = score + 50;
+            scoreText.text = "Score : " + score.ToString();
+        }
+        else if (collision.gameObject.CompareTag("BlueGem"))
+        {
+            Destroy(collision.gameObject);
+            score = score + 20;
+            scoreText.text = "Score : " + score.ToString();
+        }
+    }
+}
